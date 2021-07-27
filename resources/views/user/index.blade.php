796803fdea50e72d98f5dc84cbbf954c3b19f910
Walberto Moncada <monposmetal@gmail.com>
2021-07-26 21:26:31 -0600

    Vista index de Users

---
+++ resources/views/user/index.blade.php
@@ -0,0 +1,50 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-EVSTQN3/azprG1Anm3QDgpJLIm9Nao0Yz1ztcQTwFspd3yD65VohhpuuCOmLASjC" crossorigin="anonymous">
+</head>
+<body>
+    <div class="conteiner">
+        <div class="row">
+            <div class="col-sm-8 mx-auto">
+
+                <table class="table">
+                    <thead>
+                        <tr>
+                            <th>ID</th>
+                            <th>Nombre</th>
+                            <th>Email</th>
+                            <th>&nbsp;</th>
+                        </tr>
+                    </thead>
+                    <tbody>
+                        @foreach($users as $user)
+                        <tr>
+                            <td>{{ $user->id }}</td>
+                            <td>{{ $user->name }}</td>
+                            <td>{{ $user->email }}</td>
+                            <td>
+                                <form action="{{ route('users.destroy', $user) }}" method="POST">
+                                    @method('DELETE')
+                                    @csrf
+                                    <input 
+                                        type="submit" 
+                                        value="Eliminar" 
+                                        class="btn btn-sm btn-danger"
+                                        onclick="return confirm('¿Desea eliminar...?')"
+                                    >
+                                </form>
+                            </td>
+                        </tr>
+                        @endforeach
+                    </tbody>
+                </table>
+            </div>
+        </div>
+    </div>
+</body>
+</html>

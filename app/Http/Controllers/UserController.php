053eaf9cd5b3dd8d0dac0f9add2fb87fcddc306a
Walberto Moncada <monposmetal@gmail.com>
2021-07-26 21:52:36 -0600

    encriptado de password

--- app/Http/Controllers/UserController.php
+++ app/Http/Controllers/UserController.php
@@ -29,7 +29,7 @@ class UserController extends Controller
         User::create([
             "name" => $request->name,
             "email" => $request->email,
-            "password" => $request->password,
+            "password" => bcrypt($request->password),
         ]);
 
         return back();

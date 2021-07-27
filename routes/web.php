3471f7acb9668451e3b08f5bd36fab3502633bfb
Walberto Moncada <monposmetal@gmail.com>
2021-07-26 20:45:55 -0600

    configuracion de rutas

--- routes/web.php
+++ routes/web.php
@@ -10,7 +10,13 @@
 | contains the "web" middleware group. Now create something great!
 |
 */
-
+/*
 Route::get('/', function () {
     return view('welcome');
 });
+*/
+
+Route::get("/", "UserController@index");
+Route::post("users", "UserController@store")->name("users.store");
+Route::delete("users/{user}", "UserController@destroy")->name("users.destroy");
+
